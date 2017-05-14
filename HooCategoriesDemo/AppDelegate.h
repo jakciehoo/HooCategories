ccf87144515cd0b4d5babd9e3e00ec3b690822e4
hujianghua017 <hujianghua@pingan.com.cn>
2017-05-14 20:45:59 +0800

    Initial Commit

---
+++ HooCategoriesDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HooCategoriesDemo
+//
+//  Created by jackiehoo on 14/05/2017.
+//  Copyright © 2017 jackiehoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
